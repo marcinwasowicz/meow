49104fe6455bf2ae1a4541d13d459768cb5a371f
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-12-16 14:26:09 +0100

    Refactor nsga_ii script to allow adding all problems mentioned in original paper

--- examples/nsga_ii.exs
+++ examples/nsga_ii.exs
@@ -6,16 +6,36 @@ Mix.install([
 
 Nx.Defn.global_default_options(compiler: EXLA)
 
-defmodule Problem do
+defmodule FonescaFleming do
   import Nx.Defn
 
-  def size, do: 1
+  def size, do: 3
+
+  def lower_bound, do: -4
+
+  def upper_bound, do: 4
+
+  def name, do: "Fonesca and Fleming"
 
   defn evaluate(genomes) do
     Nx.concatenate(
       [
-        Nx.power(genomes, 2),
-        Nx.subtract(genomes, 2) |> Nx.power(2)
+        genomes
+        |> Nx.subtract(Nx.divide(1, Nx.sqrt(3)))
+        |> Nx.power(2)
+        |> Nx.sum(axes: [-1], keep_axes: true)
+        |> Nx.multiply(-1)
+        |> Nx.exp()
+        |> Nx.multiply(-1)
+        |> Nx.add(1),
+        genomes
+        |> Nx.add(Nx.divide(1, Nx.sqrt(3)))
+        |> Nx.power(2)
+        |> Nx.sum(axes: [-1], keep_axes: true)
+        |> Nx.multiply(-1)
+        |> Nx.exp()
+        |> Nx.multiply(-1)
+        |> Nx.add(1)
       ],
       axis: 1
     )
@@ -24,28 +44,47 @@ end
 
 population_size = 100
 
-nsga_ii =
-  Meow.objective(&Problem.evaluate/1)
-  |> Meow.add_pipeline(
-    MeowNx.Ops.init_real_random_uniform(population_size, Problem.size(), -1000, 1000),
-    Meow.pipeline([
-      MeowNx.Ops.selection_fast_non_dominated_sort(true),
-      MeowNx.Ops.selection_natural(population_size),
-      Meow.Ops.split_join([
-        Meow.pipeline([
-          MeowNx.Ops.selection_tournament(1.0),
-          MeowNx.Ops.crossover_simulated_bounded_binary(0.9, -1000, 1000, 20),
-          MeowNx.Ops.mutation_bounded_polynomial(1.0, -1000, 1000, 20)
-        ]),
-        Meow.pipeline([
-          MeowNx.Ops.selection_natural(population_size)
-        ])
-      ]),
-      MeowNx.Ops.log_best_pareto_front(),
-      Meow.Ops.max_generations(20)
-    ])
-  )
+problems = [FonescaFleming]
 
-report = Meow.run(nsga_ii)
-IO.puts("\n# NSGA II Algorithm\n")
-report |> Meow.Report.format_summary() |> IO.puts()
+for problem <- problems do
+  nsga_ii =
+    Meow.objective(&problem.evaluate/1)
+    |> Meow.add_pipeline(
+      MeowNx.Ops.init_real_random_uniform(
+        population_size,
+        problem.size(),
+        problem.lower_bound(),
+        problem.upper_bound()
+      ),
+      Meow.pipeline([
+        MeowNx.Ops.selection_fast_non_dominated_sort(true),
+        MeowNx.Ops.selection_natural(population_size),
+        Meow.Ops.split_join([
+          Meow.pipeline([
+            MeowNx.Ops.selection_tournament(1.0),
+            MeowNx.Ops.crossover_simulated_bounded_binary(
+              0.9,
+              problem.lower_bound(),
+              problem.upper_bound(),
+              20
+            ),
+            MeowNx.Ops.mutation_bounded_polynomial(
+              1.0,
+              problem.lower_bound(),
+              problem.upper_bound(),
+              20
+            )
+          ]),
+          Meow.pipeline([
+            MeowNx.Ops.selection_natural(population_size)
+          ])
+        ]),
+        MeowNx.Ops.log_best_pareto_front(),
+        Meow.Ops.max_generations(250)
+      ])
+    )
+
+  report = Meow.run(nsga_ii)
+  IO.puts(problem.name())
+  report |> Meow.Report.format_summary() |> IO.puts()
+end
